1c444fa35eff57d1981cb9276b4873a445a4556d
jerson100 <juamkoo@gmail.com>
2023-01-20 12:14:18 -0500

    Modify cors

--- index.js
+++ index.js
@@ -30,11 +30,7 @@ mongoose.connection.on("error", () => {
 });
 
 const app = express();
-app.use(
-  cors({
-    origin: "*",
-  })
-);
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.raw({ type: "image/*", limit: "8mb" }));
 app.use(
